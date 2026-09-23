964e1ef48ae473b1f4bbb9d666c1119b1f0f7758
Nivya <nivyaraj2612@gmail.com>
2026-09-23 13:18:29 +0530

    Trial

--- Sample1.js
+++ Sample1.js
@@ -1,1 +1,66 @@
 console.log("hello world");
+
+
+let studentName ="Alice";
+console.log(studentName);
+
+let age_no=25;
+console.log(age_no);
+
+let name="John";
+let age=30;
+console.log(name);
+console.log(age);
+
+let country="India";
+console.log(country);
+
+console.log(typeof studentName); 
+
+let marks=35;
+console.log(marks);
+console.log(typeof marks);
+
+let age_s= 26;
+age_s=60;
+console.log(age_s);
+
+
+let isStudent=true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+
+const collegeName="ABC College";
+console.log(collegeName);
+
+
+let price=100;
+let quantity=3;
+let total = price*quantity;
+console.log(total);
+
+
+let name1="Alice"
+let age1=25
+let country1="India"
+
+let isActive=true
+console.log(isActive)
+console.log(typeof isActive);
+
+price=99.5
+console.log(typeof price);
+
+
+let result=null;
+console.log(typeof result)
+
+let city
+console.log(city)
+
+let id=Symbol("id")
+console.log(id)
+console.log(typeof id)
+
+
